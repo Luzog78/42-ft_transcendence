fa3c8d0df30b814c1111aee4b048e5f900b0499d
psalame <psalame@student.42angouleme.fr>
2024-06-10 14:10:50 +0200

    fixed autodate now api models

--- api_app/models.py
+++ api_app/models.py
@@ -4,26 +4,26 @@ from django.db import models
 # Create your models here.
 
 class users(models.Model):
-	id = models.IntegerField(primary_key=True)
-	created_at = models.DateTimeField()
+	id = models.AutoField(primary_key=True)
+	created_at = models.DateTimeField(auto_now=True, blank=False)
 	mail = models.CharField(max_length=255)
 	username = models.CharField(max_length=255)
 	password = models.CharField(max_length=255)
-	a2f = models.BooleanField()
+	a2f = models.BooleanField(default=False)
 
 class game(models.Model):
-	id = models.IntegerField(primary_key=True)
+	id = models.AutoField(primary_key=True)
 	players = ArrayField(
 		models.IntegerField(), #-> users.id
 		size=4
 	)
-	created_at = models.DateTimeField()
-	started_at = models.DateTimeField()
-	ended_at = models.DateTimeField()
+	created_at = models.DateTimeField(auto_now=True, blank=False)
+	started_at = models.DateTimeField(auto_now=False, blank=True)
+	ended_at = models.DateTimeField(auto_now=False, blank=True)
 	winner = models.IntegerField() #-> users.id
 
 class stats(models.Model):
-	id = models.IntegerField(primary_key=True)
+	id = models.AutoField(primary_key=True)
 	user_id = models.IntegerField() #-> users.id
 	game_id = models.IntegerField() #->game.id
 	scored = models.IntegerField()
